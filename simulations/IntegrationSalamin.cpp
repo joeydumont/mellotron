8edae38922058a396faf21799ef4c91e075b88e2
McNoggins <gagnon88@gmail.com>
2016-11-29 16:11:14 -0500

    Compiled initial version of simulation programs

---
+++ simulations/IntegrationSalamin.cpp
@@ -0,0 +1,23 @@
+/*! ------------------------------------------------------------------------- *
+ * \author Denis Gagnon 										              *
+ * \since 2016-11-29                                                          *
+ *                                                                            *
+ * Simulation program for election/ion trajectory computation using           *
+ * Salamin's model. Reads parameters from the command line.		              *
+ * --------------------------------------------------------------------------*/
+
+#include <armadillo>
+#include <mellotron>
+#include <boost/program_options.hpp>
+#include <boost/numeric/odeint.hpp>
+
+namespace po = boost::program_options;
+namespace odeint = boost::numeric::odeint;
+
+int main(int argc, char* argv[])
+{
+
+
+  return 0;
+
+}
