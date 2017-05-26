c0566bb7b0d6ef9fc6edde57128642634f04336d
Justine Pepin <justine.pepin@emt.inrs.ca>
2017-05-26 11:45:19 -0400

    Compilation separated from the rest

---
+++ simulations/compileAll.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# --------------------------------------------------------------------------- #
+#  \file compileAll.sh                                                        #
+#  \author Justine Pepin                  <justine.pepin@emt.inrs.ca>         #
+#  \since 2017-05-26                                                          #
+#                                                                             #
+# This bash file compile all required files for the MELLOTRON.                #
+#                                                                             #
+# Usage: ./compileAll.sh                                                      #
+# --------------------------------------------------------------------------- #
+
+# -- Check if compile.sh exists
+COMP="compile.sh"
+if [ ! -f $COMP ]; then
+echo "Usage: compileAll.sh needs compile.sh in same directory."
+exit 1
+fi
+chmod +x compile.sh
+
+# -- Compile IntegrationSalamin.cpp to have .o
+INTSALCPP="IntegrationSalamin.cpp"
+if [ ! -f $INTSALCPP ]; then
+echo "Usage: compileAll.sh needs IntegrationSalamin.cpp in same directory."
+exit 1
+fi
+./compile.sh $INTSALCPP
+echo "Done: compile IntegrationSalamin.cpp."
+
+# -- Compile ComputeNormalizationConstantsSalaminLinear.cpp to have .o
+COMPUTECPP="ComputeNormalizationConstantSalaminLinear.cpp"
+if [ ! -f $COMPUTECPP ]; then
+echo "Usage: compileAll.sh needs ComputeNormalizationConstantSalaminLinear.cpp in same directory."
+exit 1
+fi
+./compile.sh $COMPUTECPP
+echo "Done: compile ComputeNormalizationConstantSalaminLinear.cpp."
+
+# -- Check that the script can find IntegrationSalamin.o, GenerateInitialConditions.py and ComputeNormalizationConstantSalaminLinear.o
+INTSAL="IntegrationSalamin.o"
+GENINIT="GenerateInitialConditions.py"
+COMPUTE="ComputeNormalizationConstantSalaminLinear.o" 
+if [ ! -f $INTSAL ]; then
+echo "Usage: compileAll.sh needs IntegrationSalamin.o in same directory."
+exit 1
+fi
+if [ ! -f $GENINIT ]; then
+echo "Usage: compileAll.sh needs GenerateInitialConditions.py in same directory."
+exit 1
+fi
+if [ ! -f $COMPUTE ]; then
+echo "Usage: compileAll.sh needs ComputeNormalizationConstantSalaminLinear.o in same directory."
+exit 1
+fi
+
+# -- Compute normalization constant
+OUTNORMCONST="normalization_constant.txt"
+./ComputeNormalizationConstantSalaminLinear.o --outfile $OUTNORMCONST
+echo "Done: compute normalization constant."
+
+
