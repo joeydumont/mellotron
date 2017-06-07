f2a34a3f85f75f1c227089b46b0a68a1d5031481
Justine Pepin <justine.pepin@emt.inrs.ca>
2017-06-07 11:17:59 -0400

    Plot stops at specified timestep

--- simulations/producePlots.py
+++ simulations/producePlots.py
@@ -16,26 +16,28 @@ mpl.rcParams['text.usetex'] = True
 mpl.rcParams['font.family'] = 'serif'
 
 
-def createOneParticleTrajectory(directory, hdf5File):
+def findFileInDir(directory, fileName):
     # Find the .hdf5 file
     globalModel = ""
     for file in os.listdir(directory):
-        if file == hdf5File:
+        if file == fileName:
             globalModel = file
             break
 
     if globalModel == "":
         print("It seems like the folder you gave doesn\'t have the specified .hdf5 file in it.")
         sys.exit()
-    
-    # Determine exactly how many TimeSteps there are.
-    globalModelFile = hp.File(directory + globalModel, "r")
-    globalModelGroup = globalModelFile.require_group(globalModel)
-    globalModelTimes = globalModelGroup["times"]
-    nTimeSteps = globalModelTimes.len()
 
+    return globalModel
     
-    # Create positions plot
+def setPositionsPlotLabels(graph):
+    graph.set_xlabel(r"X Axis")
+    graph.set_ylabel(r"Y Axis")
+    graph.set_zlabel(r"Z Axis")
+    graph.set_title(r"Positions of Particles")
+
+def createOneParticleTrajectory(directory, hdf5File, nTimeSteps, globalModelGroup):
+    # Open position hdf5 table
     globalModelPositions = globalModelGroup["position"]
     # Initiate arrays of good size.
     xp = np.empty((nTimeSteps))
@@ -51,10 +53,7 @@ def createOneParticleTrajectory(directory, hdf5File):
     ax = fig.gca(projection='3d')
 
     ax.plot(xp, yp, zs=zp, lw=0.5)
-    ax.set_xlabel(r"X Axis")
-    ax.set_ylabel(r"Y Axis")
-    ax.set_zlabel(r"Z Axis")
-    ax.set_title(r"Positions of Particles")
+    setPositionsPlotLabels(ax)
     
     plt.savefig(directory + os.path.splitext(hdf5File)[0] + ".eps")
     sys.exit()
@@ -64,8 +63,6 @@ def createPositionsPlot(globalModelPositions, nParticles, nTimeSteps, directory)
     xp = np.empty((nParticles, nTimeSteps))
     yp = np.empty((nParticles, nTimeSteps))
     zp = np.empty((nParticles, nTimeSteps))
-
-
     # Stepping through the Particles and the TimeSteps.
     for j in range(nParticles):
         for i in range(nTimeSteps):
@@ -78,20 +75,18 @@ def createPositionsPlot(globalModelPositions, nParticles, nTimeSteps, directory)
 
     for j in range(nParticles):
         ax.plot(xp[j], yp[j], zs=zp[j], lw=0.5)
-    ax.set_xlabel(r"X Axis")
-    ax.set_ylabel(r"Y Axis")
-    ax.set_zlabel(r"Z Axis")
-    ax.set_title(r"Positions of Particles")
+    setPositionsPlotLabels(ax)
 
     plt.savefig(directory + "positionsPlot.eps")
 
 
 def createPolarGammaPlot(globalModelMomentums, globalModelGamma, nParticles, nTimeSteps, directory):
+    # Initiate arrays of good size.
     r = np.empty((nParticles))
     theta = np.empty((nParticles))
     phi = np.empty((nParticles))
     maxGamma = 0.0
-
+    # Stepping through the Particles.
     for j in range(nParticles):
             r[j] = globalModelGamma[nTimeSteps - 1, 0, j]
             if r[j] > maxGamma:
@@ -117,6 +112,7 @@ def createPolarGammaPlot(globalModelMomentums, globalModelGamma, nParticles, nTi
     ax2.set_title(r"yx plane ($\phi$)", va='bottom')
     ax2.set_ylabel(r"gamma", labelpad=30)
 
+    # Put multicolored bands
     N, bins, patches = ax3.hist(r, bins=int(np.ceil(1.5*np.sqrt(nParticles))), color=[0.8, 0.8, 0.2])
     for i in range(len(patches)):
         patches[i].set_facecolor((np.random.random(1)[0], np.random.random(1)[0], np.random.random(1)[0]))
@@ -139,6 +135,8 @@ def main():
                         help="Target directory containing a .hdf5 file.")
     parser.add_argument("--file", type=str,   default="global.hdf5",
                         help=".hdf5 file with 1 particle to generate the trajectory plot with.")
+    parser.add_argument("--nTimeSteps", type=int,   default=0,
+                        help="Number of timeSteps to plot on. If 0, the maximal number of timeSteps will be used. Default is the maximum.")
 
     # Parse arguments
     args = parser.parse_args()
@@ -151,24 +149,24 @@ def main():
     # Target file
     hdf5File = args.file
     if hdf5File != "global.hdf5":
-        createOneParticleTrajectory(directory, hdf5File)
+        globalModel = findFileInDir(directory, hdf5File)
+    else:
+        globalModel = findFileInDir(directory, "global.hdf5")
 
-    # Find the global.hdf5 file
-    globalModel = ""
-    for file in os.listdir(directory):
-        if file == "global.hdf5":
-            globalModel = file
-            break
-
-    if globalModel == "":
-        print("It seems like the folder you gave doesn\'t have a global.hdf5 file in it.")
-        sys.exit()
-    
     # Determine exactly how many TimeSteps there are.
     globalModelFile = hp.File(directory + globalModel, "r")
     globalModelGroup = globalModelFile.require_group(globalModel)
     globalModelTimes = globalModelGroup["times"]
     nTimeSteps = globalModelTimes.len()
+
+    # Target nTimeSteps
+    targetTime = args.nTimeSteps
+    if targetTime != 0 and targetTime < nTimeSteps:
+        nTimeSteps = targetTime
+
+    # Il only one particle, no need to do rest of main 
+    if hdf5File != "global.hdf5":
+        createOneParticleTrajectory(directory, hdf5File, nTimeSteps, globalModelGroup)
 
     # Determine exactly how many Particles there are.
     globalModelPositions = globalModelGroup["position"]
