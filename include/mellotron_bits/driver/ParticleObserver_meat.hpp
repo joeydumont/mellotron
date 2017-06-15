5a1bb0fa8f284007d38d93e43154f531fe166779
Justine Pepin <justine.pepin@emt.inrs.ca>
2017-06-15 15:30:46 -0400

    Gamma est toujours >= 1

--- include/mellotron_bits/driver/ParticleObserver_meat.hpp
+++ include/mellotron_bits/driver/ParticleObserver_meat.hpp
@@ -25,7 +25,7 @@ ParticleObserver<FieldModel>::operator() (const arma::colvec::fixed<8> &x,
   // Push the data to the appropriate containers.
   position.col(n_cols) = x.subvec(1,3);
   momentum.col(n_cols) = x.subvec(5,7);
-  gamma.push_back(x[4]/particle.GetMass());
+  gamma.push_back(std::sqrt(1.0+std::pow(arma::norm(momentum,2)/particle.GetMass(),2)));
   times.push_back(x[0]);
 
   // Compute the electromagnetic field and store it.
