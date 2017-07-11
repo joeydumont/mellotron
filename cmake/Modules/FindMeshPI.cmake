a62c09d16ca8a7e0fb5c300c5cf2b6e0d43b430e
Justine Pepin <justine.pepin@emt.inrs.ca>
2017-07-11 13:57:36 -0400

    good convention names in .cmake

--- cmake/Modules/FindMeshPI.cmake
+++ cmake/Modules/FindMeshPI.cmake
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------------------- #
 # Author:       Denis Gagnon               <denis.gagnon@emt.inrs.ca>         #
 # Date:         2017-06-07                                                    #
-# Description:  CMake module to find MeshPI.                                  #
+# Description:  CMake module to find meshpi.                                  #
 # ----
 
 # -- LibFindMacros for convenience
@@ -9,21 +9,21 @@
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(MeshPI_PKGCONF MeshPI)
+libfind_pkg_check_modules(meshpi_PKGCONF meshpi)
 
 # Include dir
-find_path(MeshPI_INCLUDE_DIR
-  NAMES MeshPI
-  PATHS ${MeshPI_PKGCONF_INCLUDE_DIRS}
+find_path(meshpi_INCLUDE_DIR
+  NAMES meshpi
+  PATHS ${meshpi_PKGCONF_INCLUDE_DIRS}
 )
 
 # Finally the library itself
-find_library(MeshPI_LIBRARY
+find_library(meshpi_LIBRARY
     NAMES libMeshPI MeshPI libMeshPI.so libMeshPI.dylib
-    PATHS ${MeshPI_PKGCONF_LIBRARY_DIRS}
+    PATHS ${meshpi_PKGCONF_LIBRARY_DIRS}
 )
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
-set(MeshPI_PROCESS_INCLUDE MeshPI_INCLUDE_DIR)
-set(MeshPI_PROCESS_LIB MeshPI_LIBRARY)
-libfind_process(MeshPI)
+set(meshpi_PROCESS_INCLUDE meshpi_INCLUDE_DIR)
+set(meshpi_PROCESS_LIB meshpi_LIBRARY)
+libfind_process(meshpi)
--- cmake/Modules/FindStrattoCalculator.cmake
+++ cmake/Modules/FindStrattoCalculator.cmake
@@ -1,29 +1,29 @@
 # --------------------------------------------------------------------------- #
 # Author:       Denis Gagnon               <denis.gagnon@emt.inrs.ca>         #
 # Date:         2017-06-07                                                    #
-# Description:  CMake module to find StrattoCalculator.                       #
-# ----
+# Description:  CMake module to find strattocalculator.                       #
+# --------------------------------------------------------------------------- #
 
 # -- LibFindMacros for convenience
 # https://github.com/Tronic/cmake-modules/blob/master/LibFindMacros.cmake
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(StrattoCalculator_PKGCONF StrattoCalculator)
+libfind_pkg_check_modules(strattocalculator_PKGCONF strattocalculator)
 
 # Include dir
-find_path(StrattoCalculator_INCLUDE_DIR
-  NAMES StrattoCalculator
-  PATHS ${StrattoCalculator_PKGCONF_INCLUDE_DIRS}
+find_path(strattocalculator_INCLUDE_DIR
+  NAMES strattocalculator
+  PATHS ${strattocalculator_PKGCONF_INCLUDE_DIRS}
 )
 
 # Finally the library itself
-find_library(StrattoCalculator_LIBRARY
+find_library(strattocalculator_LIBRARY
     NAMES libStrattoCalculator StrattoCalculator libStrattoCalculator.so libStrattoCalculator.dylib
-    PATHS ${StrattoCalculator_PKGCONF_LIBRARY_DIRS}
+    PATHS ${strattocalculator_PKGCONF_LIBRARY_DIRS}
 )
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
-set(StrattoCalculator_PROCESS_INCLUDE StrattoCalculator_INCLUDE_DIR)
-set(StrattoCalculator_PROCESS_LIB StrattoCalculator_LIBRARY)
-libfind_process(StrattoCalculator)
+set(strattocalculator_PROCESS_INCLUDE strattocalculator_INCLUDE_DIR)
+set(strattocalculator_PROCESS_LIB strattocalculator_LIBRARY)
+libfind_process(strattocalculator)
